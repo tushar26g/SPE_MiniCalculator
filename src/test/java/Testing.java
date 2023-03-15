efc2f9e55fe2ecbd9a0a38524202f569d1f5f1f1
tushar26g <tushar26g@gmail.com>
2023-03-15 16:36:54 +0530

    add test cases for factorial functionality

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -6,6 +6,24 @@ import org.junit.Test;
 public class Testing {
     private static final double DELTA = 1e-15;
     Calculator calculator = new Calculator();
+
+    @Test
+    public void factorialTruePositive(){
+        assertEquals("Finding factorial of a number for True Positive", 720, calculator.fact(6), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 1, calculator.fact(1), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 6, calculator.fact(3), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 24, calculator.fact(4), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 1, calculator.fact(0), DELTA);
+    }
+
+    @Test
+    public void factorialFalsePositive(){
+        assertNotEquals("Finding factorial of a number for False Positive", 113, calculator.fact(5), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 10, calculator.fact(6), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 42, calculator.fact(4), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 9, calculator.fact(2), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 0, calculator.fact(0), DELTA);
+    }
     @Test
     public void squareRootTruePositive(){
         assertEquals("Finding square root for True Positive", 5, calculator.sqroot(25), DELTA);
