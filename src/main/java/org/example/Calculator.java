f1b95233eb8786072b52d6d50971651f5cd1f28f
tushar26g <tushar26g@gmail.com>
2023-03-15 16:29:03 +0530

    add factorial functionality

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -13,7 +13,7 @@ public class Calculator {
         double number1, number2;
         do {
             System.out.println("Calculator-DevOps, Choose to perform operation");
-            System.out.print("Press 1 to find Square root\nEnter your choice: ");
+            System.out.print("1. Factorial\n2. Square root\nEnter your choice: ");
             int choice;
             try {
                 choice = scanner.nextInt();
@@ -23,6 +23,14 @@ public class Calculator {
 
             switch (choice) {
                 case 1:
+                    // Factorial
+                    System.out.print("Enter a number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.println("Factorial of "+number1+" is : " + calculator.factorial(number1));
+                    System.out.println("\n");
+
+                    break;
+                case 2:
                     // do factorial
                     System.out.print("Enter a number : ");
                     number1 = scanner.nextDouble();
@@ -35,6 +43,18 @@ public class Calculator {
             }
         } while (true);
     }
+    public double factorial(double number1) {
+        double result = fact(number1);
+        return result;
+    }
+
+    public double fact(double num) {
+        double facto = 1;
+        for(int i = 1; i <= num; ++i)
+        { facto *= i;   }
+        return  facto;
+    }
+
     public double sqroot(double number1) {
         double result = Math.sqrt(number1);
         return result;
